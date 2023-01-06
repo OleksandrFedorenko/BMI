7ca8eee856415be7733714f8c26312642aec9851
Summer <afedorenko02@gmail.com>
2023-01-06 12:06:25 +0200

    add some documentation

--- BMI/Model/CalculatorBrain.swift
+++ BMI/Model/CalculatorBrain.swift
@@ -10,29 +10,34 @@ import UIKit
 class CalculateBraine{
 
     var bmiValue: BMI?
-
+    
+    /// Function that calculate BMI value according to the passed parameters such as height and weight. Also fullfill BMI structer according to the value we have  calculated namly value, advice, and color.
+    /// - Parameters:
+    ///   - height: taken from the heightSlider
+    ///   - weight: taken frome the weightSlider
     func calculateBMI(height: Float, weight: Float) {
         print("\(height) - \(weight)")
         let bmi = weight / (height * height)
         if bmi < 18.5{
-            bmiValue = BMI(value: bmi, advice: "Underweight", color: UIColor(red: 253.0/255, green: 138.0/255, blue: 138.0/255, alpha: 1.0))
+            bmiValue = BMI(value: bmi, advice: "Underweight", color: UIColor(red: 255/255, green: 202/255, blue: 200/255, alpha: 1.0))
         }else if bmi < 24.9{
-            bmiValue = BMI(value: bmi, advice: "Normal", color: UIColor(red: 0.0, green: 129.0/255, blue: 201.0/255, alpha: 1.0))
+            bmiValue = BMI(value: bmi, advice: "Normal", color: UIColor(red: 192/255, green: 238/255, blue: 228/255, alpha: 1.0))
         }else{
-            bmiValue = BMI(value: bmi, advice: "Overweight", color: UIColor(red: 251.0/255, green: 194.0/255, blue: 82.0/255, alpha: 1))
+            bmiValue = BMI(value: bmi, advice: "Overweight", color: UIColor(red: 251/255, green: 194/255, blue: 82/255, alpha: 1))
         }
     }
-
+    
+    /// Downcasting float bmi value  to the string type using nil coalescing approach
+    /// - Returns: String type of BMI value
     func getBMIValue() -> String {
         let valueToString = String(format: "%.2f", bmiValue?.value ?? "0.0")// nil coalescing
-        print("bmi - \(valueToString)")
         return valueToString
     }
     
     func getAdvice() -> String {
         return bmiValue?.advice ?? "got nil in getAdvice"
     }
-    
+
     func getColor() -> UIColor {
         if let safeColor = bmiValue?.color{
             return safeColor
@@ -41,4 +46,3 @@ class CalculateBraine{
         }
     }
 }
-
